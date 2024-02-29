879ebebed05a5d205fd66d0be9037d9c6fd7a690
Cliff <134680422+Finn9928@users.noreply.github.com>
2024-02-29 15:08:21 +1300

    Update 0.1.1 DinoClone.js
    added broken cubes

--- DinoClone.js
+++ DinoClone.js
@@ -6,8 +6,9 @@ console.log("Debug");
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT = 400;
 const PLAYER_SCALE = SCREEN_HEIGHT/8;
-
-
+const BLOCK_SCALE = SCREEN_HEIGHT/8;
+var blockSpeed = -10;
+var blocks;
 /*******************************************************/
 // setup()
 /*******************************************************/
@@ -16,6 +17,7 @@ function setup() {
     cnv = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
     blueCube= new Sprite(100, 100, PLAYER_SCALE, PLAYER_SCALE, 'd');
     blueCube.color = 'blue';
+    blocks = new Group();
     ground = new Sprite(SCREEN_WIDTH/2, SCREEN_HEIGHT, SCREEN_WIDTH, 4, 's');
     ground.color = 'black';
     world.gravity.y = 80;
@@ -23,11 +25,14 @@ function setup() {
         function(event) {
             console.log("Key pressed!"+blueCube.y);
 
-            if(blueCube.y > 369 ){// 184 - found from testing - floor level
+            if(blueCube.y > 356 ){// 184 - found from testing - floor level
                 console.log("Key pressed!"+blueCube.y);
                 blueCube.vel.y = -20;
             }
     });
+    blueCube.collides(blocks, deathOfPlayer);
+    
+
 }
 
 /*******************************************************/
@@ -35,7 +40,16 @@ function setup() {
 /*******************************************************/
 function draw() {
     background('cyan');
-    
+    newBlock();
+}
+function newBlock(){
+    block= new Sprite(600,374, BLOCK_SCALE, BLOCK_SCALE, 'k');
+    block.color = 'lightGreen';
+    block.vel.x = blockSpeed ;
+    blocks.add(block);
+}
+function deathOfPlayer(_blueCube, _block){
+    console.log("playerhit");
 }
 /*******************************************************/
 //  END OF APP
